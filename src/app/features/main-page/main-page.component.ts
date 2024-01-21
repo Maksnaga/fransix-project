8543ba030f15a83d9d58a0be942901362940a524
Maksnaga <110107260+Maksime59@users.noreply.github.com>
2024-01-21 20:22:18 +0100

    WIP

--- src/app/features/main-page/main-page.component.ts
+++ src/app/features/main-page/main-page.component.ts
@@ -1,11 +1,12 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { RouterModule } from '@angular/router';
+import { TagModule } from 'primeng/tag';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [ButtonModule, RouterModule],
+  imports: [ButtonModule, RouterModule, TagModule],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss',
 })
@@ -13,7 +14,7 @@ export class MainPageComponent implements AfterViewInit {
   @ViewChild('main') mainSection!: ElementRef;
   @ViewChild('service') serviceSection!: ElementRef;
   @ViewChild('who') whoSection!: ElementRef;
-
+  private isClicked = false;
   public activeMenu = 'Accueil';
   public homePageMenu = [
     {
@@ -32,10 +33,39 @@ export class MainPageComponent implements AfterViewInit {
 
   ngAfterViewInit(): void {
     this.mainSection.nativeElement.scrollIntoView({ behavior: 'smooth' });
+    this.observeSection(this.mainSection);
+    this.observeSection(this.serviceSection);
+    this.observeSection(this.whoSection);
+  }
+
+  private observeSection(section: ElementRef): void {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (this.isClicked) {
+          // Ignorez l'intersection si le menu a été cliqué
+          return;
+        }
+
+        const menuOption = this.homePageMenu.find(
+          (option) => option.id === section.nativeElement.id
+        );
+
+        if (entry.isIntersecting) {
+          this.activeMenu = menuOption?.name || '';
+        }
+      },
+      { threshold: 0.7 }
+    );
+
+    observer.observe(section.nativeElement);
   }
 
   public handleMenuItemClick(name: string): void {
     this.activeMenu = name;
+    this.isClicked = true;
+    setTimeout(() => {
+      this.isClicked = false;
+    }, 1000);
   }
 
   public onClick(elementId: string): void {
